6fc295ed2d64c113f70595e8441830cd7fdc1ac2
user1 <backenduz@gmail.com>
2022-11-14 23:42:30 +0500

    ikki

--- is_odd.py
+++ is_odd.py
@@ -1,6 +1,7 @@
 #Create a variable "number" and assign it a integer value.
-a=19
-x=a-(a//2)
-y=x-1
-print(a-x-y)
+#a=15
+#x=a-(a//2) #7
+#y=x-1 #6
+#print(x-y) #13-7-6=1
 #Print 1 if the number is odd, otherwise print 0.
+print(13//2)
