ebb391ce33541925a44d484949cc68183f109e1e
user1 <backenduz@gmail.com>
2022-11-14 23:09:29 +0500

    bir

--- is_even.py
+++ is_even.py
@@ -1,6 +1,6 @@
 #Create a variable "number" and assign it a integer value.
-a=10
-x=a-(a//2)
-y=x-1
-print(a-x-y)
+a=13
+x=a-(a//2) #7
+y=x-1 #6
+print(a-x-y) #13-7-6=1
 #Print 1 if the number is even, otherwise print 0.
--- is_odd.py
+++ is_odd.py
@@ -1,3 +1,6 @@
 #Create a variable "number" and assign it a integer value.
-
+a=19
+x=a-(a//2)
+y=x-1
+print(a-x-y)
 #Print 1 if the number is odd, otherwise print 0.
